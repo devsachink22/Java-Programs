07cd2dfd363327b6246cabe27ada82ab43b5d74c
Sachin Kumar <67166839+devsachink22@users.noreply.github.com>
2025-05-23 23:02:17 +0530

    Data-Types

---
+++ Data-Types/NonNumeric.java
@@ -0,0 +1,37 @@
+class NonMumeric
+{
+    public static void main(String[] args)
+    {
+        boolean b=false; //initialization of boolean
+        System.out.println(b);
+
+        char c='s'; //initialization of character
+        System.out.println(c);
+
+        byte bl=-128; //minimum limit of byte
+        System.out.println(bl);
+        byte bh=127; //maximum limit of byte
+        System.out.println(bh);
+
+        short sl=-32768; //minimum limit of short
+        System.out.println(sl);
+        short sh=32767; //maximum limit of short
+        System.out.println(sh);
+
+        int il=-2147483648; //minimum limit of integer
+        System.out.println(il);
+        int ih=2147483647; //maximum limit of integer
+        System.out.println(ih);
+
+        long ll=-9223372036854775808L; //minimum limit of long
+        System.out.println(ll);
+        long lh=9223372036854775807L; //maximum limit of long
+        System.out.println(lh);
+
+        float f=12.76f; //initialization of float
+        System.out.println(f);
+
+        double d=1737.76f; //initialization of double
+        System.out.println(d);
+    }
+}
